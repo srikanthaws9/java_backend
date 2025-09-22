44d1afdfe5e41bde32466a6ad5ecc2ad861ee635
“Srikanth <cvssrikanth@gmail.com>
2025-09-22 22:52:02 +0530

    hallpass.tf has updaged tags

--- hallpass.tf
+++ hallpass.tf
@@ -43,5 +43,7 @@ resource "aws_s3_bucket" "s3_bucket" {
         Environment = local.Env
         ProjectOwner = local.prodj_owner
         BillingId = local.BillingId
+        Owner = "John Dixon"
+        mail = "fsk.support@example.com"
     }
 }
